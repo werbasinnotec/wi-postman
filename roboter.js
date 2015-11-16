0236e8db25bcc2b7ab1358e57b4472a8b6a89734
mwiesmueller <martin.wiesmueller@werbas-ag.com>
2015-11-16 08:28:39 +0100

    Implementate Roboter and esLint Rules

---
+++ roboter.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var roboter = require('roboter');
+
+roboter.
+  workOn('server').
+  equipWith(function (task) {
+    task('universal/analyze', {
+      src: [ '**/*.js', '!node_modules/**/*.js' ],
+      rules: '.eslintrc'
+    });
+
+    task('universal/test-units', {
+      src: 'test/**/*Tests.js'
+    });
+  }).
+  start();
